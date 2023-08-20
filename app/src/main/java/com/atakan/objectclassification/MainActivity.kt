e970877f320187273b9c02aef11d4854a6943674
Atakan Akın <atakanakink@icloud.com>
2023-08-20 23:49:21 +0300

    handle person

--- app/src/main/java/com/atakan/objectclassification/MainActivity.kt
+++ app/src/main/java/com/atakan/objectclassification/MainActivity.kt
@@ -2,7 +2,11 @@ package com.atakan.objectclassification
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,20 +16,21 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.atakan.objectclassification.ui.theme.ObjectClassificationTheme
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.task.core.BaseOptions
 import org.tensorflow.lite.task.vision.detector.Detection
 import org.tensorflow.lite.task.vision.detector.ObjectDetector
 import org.tensorflow.lite.task.vision.detector.ObjectDetector.ObjectDetectorOptions
-
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : ComponentActivity() {
 
     lateinit var imageBitmap: Bitmap
+
+    lateinit var originalFileName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +40,8 @@ class MainActivity : ComponentActivity() {
 
         // Initialize
         val options = ObjectDetectorOptions.builder()
-            .setBaseOptions(BaseOptions.builder().useGpu().build())
-            .setMaxResults(1)
+            .setBaseOptions(BaseOptions.builder()//.useGpu() delete comment to use gpu
+                .build())
             .build()
         val objectDetector = ObjectDetector.createFromFileAndOptions(
             this, modelFileName, options
@@ -53,13 +58,56 @@ class MainActivity : ComponentActivity() {
                         Button(onClick = { pickImageFromGallery() }) {
                             Text("Choose")
                         }
+                        // Inside your onClick block
                         Button(onClick = {
                             val tensorImage = TensorImage.fromBitmap(imageBitmap)
                             val results: List<Detection> = objectDetector.detect(tensorImage)
-                            println(results)
+
+                            val personDetections = results.filter { detection ->
+                                detection.categories[0].label.equals("person", ignoreCase = true)
+                            }
+
+                            var mutableBitmap = imageBitmap.copy(imageBitmap.config, true)
+
+                            if (personDetections.isNotEmpty()) {
+                                personDetections.forEach(){it
+                                    val personBoundingBox =
+                                        it.boundingBox // Assuming you want to blur the first detected person
+
+
+                                    val canvas = Canvas(mutableBitmap)
+
+                                    val blurPaint = Paint()
+                                    blurPaint.color = Color.argb(128, 0, 0, 0)
+                                    blurPaint.style = Paint.Style.FILL_AND_STROKE
+
+                                    canvas.drawRect(personBoundingBox, blurPaint)
+
+                                }
+                                // Save the modified image to the Downloads folder
+                                val downloadsDir =
+                                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+
+                                val fileName = "${originalFileName.substringBeforeLast('.')}_edited.png" // Append "_edited" before the extension
+                                val outputPath = File(downloadsDir, fileName)
+                                val outputStream = FileOutputStream(outputPath)
+                                mutableBitmap.compress(
+                                    Bitmap.CompressFormat.PNG,
+                                    100,
+                                    outputStream
+                                )
+                                outputStream.close()
+
+                                println("image saved to: ${outputPath.absolutePath}")
+                            } else {
+                                println("No person detected in the image.")
+                            }
+
                         }) {
                             Text("Detect")
                         }
+
+
                     }
                 }
             }
@@ -67,6 +115,7 @@ class MainActivity : ComponentActivity() {
     }
     // In your activity/fragment
     private val REQUEST_IMAGE_PICK = 2
+
 
     private fun pickImageFromGallery() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
@@ -78,22 +127,10 @@ class MainActivity : ComponentActivity() {
         if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK && data != null) {
             val selectedImageUri = data.data
             imageBitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImageUri)
+            originalFileName = File(selectedImageUri!!.path!!).name // Get the original file name
 
             // Handle the detection results
         }
     }
 
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ObjectClassificationTheme {
-        Greeting("Android")
-    }
 }
